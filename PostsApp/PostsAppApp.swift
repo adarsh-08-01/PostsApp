9698b8b619e5cc22fe926e3c9b66cfd10cca756d
adarsh-08-01 <230691@kit.ac.in>
2026-09-10 03:01:09 +0530

    Initial Commit

---
+++ PostsApp/PostsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostsAppApp.swift
+//  PostsApp
+//
+//  Created by adarsh Kashyap on 10/09/26.
+//
+
+import SwiftUI
+
+@main
+struct PostsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
